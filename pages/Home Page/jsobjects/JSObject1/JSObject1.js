1db045ef1b5f2084e47aba98c4c7a87a4de45760
Dinithi Rathnayake <dinithinayanamali98@gmail.com>
2024-01-18 04:55:12 +0000

    System generated commit, initial commit

---
+++ pages/Home Page/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,14 @@
+export default {
+	defHomeImgUrl:"https://i.ibb.co/KGS61GC/65-removebg-preview.png",
+	defThumbImgUrl:"https://i.ibb.co/3S3d3K4/881765040.png",
+	getText: (Platform) => {
+		if (Platform == 'Instagram') return "lightsalmon";
+		else if (Platform == 'Spotify') return "lightgreen";
+		else if (Platform == 'YouTube') return "lightcoral";
+		else if (Platform == 'Spotify') return "lavender";
+		else if (Platform == 'Facebook') return "lightblue";
+		else if (Platform == 'Reddit') return "lightcyan";
+		else return "lightslategray";
+	}
+
+}
